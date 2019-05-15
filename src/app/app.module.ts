0f7dbeadcd13cfea75943ff3593e6e676e0dbb1e
katesm <michaelkates@Michaels-MacBook-Pro.local>
2019-05-15 12:36:05 -0400

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatInputModule, MatFormFieldModule, MatTableModule, MatPaginatorModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule, MatCheckboxModule, MatToolbarModule,MatCardModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatTableModule,MatPaginatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
